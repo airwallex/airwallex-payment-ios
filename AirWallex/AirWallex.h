9764281c4ca97acb5ad5b3e8396908c65d5cb94d
Victor Zhu <victor@interactivelabs.co>
2020-01-07 13:57:34 +0800

    Base Project

---
+++ AirWallex/AirWallex.h
@@ -0,0 +1,19 @@
+//
+//  AirWallex.h
+//  AirWallex
+//
+//  Created by Victor Zhu on 2020/1/7.
+//  Copyright © 2020 AirWallex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AirWallex.
+FOUNDATION_EXPORT double AirWallexVersionNumber;
+
+//! Project version string for AirWallex.
+FOUNDATION_EXPORT const unsigned char AirWallexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AirWallex/PublicHeader.h>
+
+
