99b97215b3fdef99c9c190d875359eb8b9371418
Victor Zhu <victor@interactivelabs.co>
2020-02-26 08:25:34 +0800

    Clean

---
+++ Airwallex/Sources/API/AWPaymentIntentResponse.h
@@ -0,0 +1,66 @@
+//
+//  AWPaymentintentResponse.h
+//  Airwallex
+//
+//  Created by Victor Zhu on 2020/1/16.
+//  Copyright © 2020 Airwallex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AWResponseProtocol.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class AWConfirmPaymentNextAction;
+
+@interface AWConfirmPaymentIntentResponse : NSObject <AWResponseProtocol>
+
+@property (nonatomic, readonly) NSString *status;
+@property (nonatomic, readonly) AWConfirmPaymentNextAction *nextAction;
+
+@end
+
+@class AWWechatPaySDKResponse;
+
+@interface AWConfirmPaymentNextAction : NSObject
+
+@property (nonatomic, readonly) NSString *type;
+@property (nonatomic, readonly) AWWechatPaySDKResponse *wechatResponse;
+
++ (AWConfirmPaymentNextAction *)parse:(NSDictionary *)json;
+
+@end
+
+@interface AWWechatPaySDKResponse: NSObject
+
+@property (nonatomic, readonly, nullable) NSString *appId;
+@property (nonatomic, readonly) NSString *timeStamp;
+@property (nonatomic, readonly) NSString *nonceStr;
+@property (nonatomic, readonly) NSString *prepayId;
+@property (nonatomic, readonly) NSString *partnerId;
+@property (nonatomic, readonly) NSString *package;
+@property (nonatomic, readonly) NSString *sign;
+
++ (AWWechatPaySDKResponse *)parse:(NSDictionary *)json;
+
+@end
+
+@interface AWGetPaymentIntentResponse : NSObject <AWResponseProtocol>
+
+@property (nonatomic, readonly) NSString *Id;
+@property (nonatomic, readonly) NSString *requestId;
+@property (nonatomic, readonly) NSNumber *amount;
+@property (nonatomic, readonly) NSString *currency;
+@property (nonatomic, readonly) NSString *merchantOrderId;
+@property (nonatomic, readonly) NSObject *order;
+@property (nonatomic, readonly) NSString *descriptor;
+@property (nonatomic, readonly) NSString *status;
+@property (nonatomic, readonly) NSNumber *capturedAmount;
+@property (nonatomic, readonly) NSObject *latestPaymentAttempt;
+@property (nonatomic, readonly) NSString *createdAt;
+@property (nonatomic, readonly) NSString *updatedAt;
+@property (nonatomic, readonly) NSArray *availablePaymentMethodTypes;
+
+@end
+
+NS_ASSUME_NONNULL_END
