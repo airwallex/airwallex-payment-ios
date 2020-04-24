f6609ee7af7a74b7ffcc8a18a9b5a3e333dc13ce
Victor Zhu <victor@interactivelabs.co>
2020-04-24 16:30:53 +0800

    Update podfile

--- Pods/Target Support Files/Pods-AirwallexTests/Pods-AirwallexTests-frameworks.sh
+++ Pods/Target Support Files/Pods-AirwallexTests/Pods-AirwallexTests-frameworks.sh
@@ -197,12 +197,12 @@ if [ -r "${ARTIFACT_LIST_FILE}" ]; then
 fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Keys/Keys.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SVProgressHUD/SVProgressHUD.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Keys/Keys.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Keys/Keys.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SVProgressHUD/SVProgressHUD.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Keys/Keys.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
