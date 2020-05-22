806243ccd7d6b2853a72b6f42982a56bc5a8857c
Victor Zhu <victor@interactivelabs.co>
2020-05-22 10:57:46 +0800

    Disable zoom

--- AirWallex/Airwallex/Sources/UI/AWWebViewController.m
+++ AirWallex/Airwallex/Sources/UI/AWWebViewController.m
@@ -31,7 +31,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    NSString *script = @"var meta = document.createElement('meta'); meta.setAttribute('name', 'viewport'); meta.setAttribute('content', 'width=device-width'); document.getElementsByTagName('head')[0].appendChild(meta);";
+    NSString *script = @"var meta = document.createElement('meta'); meta.setAttribute('name', 'viewport'); meta.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'); document.getElementsByTagName('head')[0].appendChild(meta);";
     WKUserScript *userScript = [[WKUserScript alloc] initWithSource:script injectionTime:WKUserScriptInjectionTimeAtDocumentEnd forMainFrameOnly:YES];
     WKUserContentController *userContent = [[WKUserContentController alloc] init];
     [userContent addUserScript:userScript];
