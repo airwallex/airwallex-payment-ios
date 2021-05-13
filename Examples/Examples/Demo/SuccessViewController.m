44c0fe069ca33473c3209c25504f854cc0ea9756
roger <roger@interactivelabs.co>
2021-05-13 20:43:19 +0800

    Integrate h5 demo

---
+++ Examples/Examples/Demo/SuccessViewController.m
@@ -0,0 +1,33 @@
+//
+//  SuccessViewController.m
+//  Examples
+//
+//  Created by Victor Zhu on 2021/5/13.
+//  Copyright © 2020 Airwallex. All rights reserved.
+//
+
+#import "SuccessViewController.h"
+
+@interface SuccessViewController ()
+
+@end
+
+@implementation SuccessViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    UILabel *lab = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, 200, 100)];
+    lab.center = self.view.center;
+    lab.text = @"Success";
+    lab.textColor = UIColor.blackColor;
+    lab.font = [UIFont systemFontOfSize:30];
+    lab.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:lab];
+    
+}
+
+
+@end
---
+++ Examples/Examples/Demo/WebViewController.h
@@ -0,0 +1,20 @@
+//
+//  WebViewController.h
+//  Examples
+//
+//  Created by Victor Zhu on 2021/5/13.
+//  Copyright © 2020 Airwallex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WebViewController : UIViewController
+
+@property(nonatomic, copy) NSString * url;
+@property(nonatomic, copy) NSString * referer;
+
+@end
+
+NS_ASSUME_NONNULL_END
