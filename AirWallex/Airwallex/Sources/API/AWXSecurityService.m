b91968ec6050d71c6924fc877d032b5909bc56f0
Victor Zhu <ghoyelo@gmail.com>
2021-07-27 17:33:35 +0800

    Fix session

--- AirWallex/Airwallex/Sources/API/AWXSecurityService.m
+++ AirWallex/Airwallex/Sources/API/AWXSecurityService.m
@@ -34,8 +34,13 @@
     self = [super init];
     if (self) {
         self.profiling = [RLTMXProfiling sharedInstance];
+        RLTMXProfilingConnections *profilingConnections = [RLTMXProfilingConnections new];
+        profilingConnections.connectionTimeout = 20;
+        profilingConnections.connectionRetryCount = 2;
         [self.profiling configure:@{
             RLTMXOrgID: AWXCyberSourceOrganizationID,
+            RLTMXProfileTimeout: @20,
+            RLTMXProfilingConnectionsInstance: profilingConnections,
         }];
     }
     return self;
@@ -44,16 +49,21 @@
 - (void)doProfile:(NSString *)intentId
        completion:(void(^)(NSString * _Nullable))completion
 {
-//#if TARGET_OS_SIMULATOR
-//    completion([UIDevice currentDevice].identifierForVendor.UUIDString);
-//#else
+#if TARGET_OS_SIMULATOR
+    completion([UIDevice currentDevice].identifierForVendor.UUIDString);
+#else
     NSString *fraudSessionId = [NSString stringWithFormat:@"%@%f", intentId, [[NSDate date] timeIntervalSince1970]];
     NSString *sessionId = [NSString stringWithFormat:@"%@%@", AWXCyberSourceMerchantID, fraudSessionId];
     [self.profiling profileDeviceUsing:@{@"session_id": sessionId} callbackBlock:^(NSDictionary *result) {
-        NSString *sessionId = result[RLTMXSessionID];
-        completion(sessionId ?: @"");
+        RLTMXStatusCode statusCode = [[result valueForKey:RLTMXProfileStatus] integerValue];
+        if (statusCode == RLTMXStatusCodeOk) {
+            NSString *sessionId = result[RLTMXSessionID];
+            completion(sessionId ?: @"");
+            return;
+        }
+        completion(@"");
     }];
-//#endif
+#endif
 }
 
 @end
