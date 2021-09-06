9423d514f3fe3f99531c80dcf45f4ddc1b6856b4
Victor Zhu <ghoyelo@gmail.com>
2021-09-06 16:35:35 +0800

    Share pods

---
+++ Pods/Target Support Files/Pods-Examples-Production/Pods-Examples-Production-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Examples_Production : NSObject
+@end
+@implementation PodsDummy_Pods_Examples_Production
+@end
---
+++ Pods/Target Support Files/Pods-WeChatPay/Pods-WeChatPay-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WeChatPay : NSObject
+@end
+@implementation PodsDummy_Pods_WeChatPay
+@end
