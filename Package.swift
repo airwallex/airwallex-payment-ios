b8d650dc86ff5863536a4ed5298aedfad8394f69
Victor Zhu <ghoyelo@gmail.com>
2021-09-07 13:34:33 +0800

    Support swift package manager

---
+++ Package.swift
@@ -0,0 +1,80 @@
+import PackageDescription
+
+let package = Package(
+    name: "Airwallex",
+    defaultLocalization: "en-us",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "Airwallex",
+            targets: ["Airwallex"]
+        ),
+        .library(
+            name: "WeChatPay",
+            targets: ["WeChatPay"]
+        ),
+        .library(
+            name: "Card",
+            targets: ["Card"]
+        ),
+        .library(
+            name: "Redirect",
+            targets: ["Redirect"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            name: "TrustDefender",
+            url: "https://github.com/ilabsvictor/TrustDefender",
+            .exact(Version(6, 1, 2))
+        ),
+        .package(
+            name: "CardinalMobile",
+            url: "https://github.com/ilabsvictor/CardinalMobile",
+            .exact(Version(2, 2, 4))
+        ),
+    ],
+    targets: [
+        .target(
+            name: "Airwallex",
+            path: "Airwallex/Airwallex/Sources",
+            exclude: [
+                "Info.plist",
+            ]
+        ),
+        .target(
+            name: "WeChatPay",
+            dependencies: [
+                .target(name: "Airwallex"),
+            ],
+            path: "Airwallex/WeChatPay",
+            exclude: [
+                "Info.plist"
+            ]
+        ),
+        .target(
+            name: "Card",
+            dependencies: [
+                .target(name: "Airwallex"),
+                .product(name: "TrustDefender", package: "TrustDefender")
+                .product(name: "CardinalMobile", package: "CardinalMobile")
+            ],
+            path: "Airwallex/Card",
+            exclude: [
+                "Info.plist"
+            ]
+        ),
+        .target(
+            name: "Redirect",
+            dependencies: [
+                .target(name: "Airwallex"),
+            ],
+            path: "Airwallex/Redirect",
+            exclude: [
+                "Info.plist",
+            ]
+        )
+    ]
+)
