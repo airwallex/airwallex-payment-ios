b6644e37e47c201ffec713f93160bae3a7e9c429
Jin Wang <jin.wang@airwallex.com>
2022-03-15 14:35:03 +1100

    chore: removed Pods from Git

--- Pods/Target Support Files/Pods-Examples-Demo/Pods-Examples-Demo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Examples_Demo : NSObject
-@end
-@implementation PodsDummy_Pods_Examples_Demo
-@end
--- Pods/Target Support Files/Pods-Examples-Production/Pods-Examples-Production-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Examples_Production : NSObject
-@end
-@implementation PodsDummy_Pods_Examples_Production
-@end
