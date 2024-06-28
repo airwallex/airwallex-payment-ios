4fb721f475b2b01f67438aece7d3d6f99ea0e388
Tony He <tony.he.ctr@airwallex.com>
2024-06-28 10:14:33 +0800

    chore: mock datatask for URLSession.

--- Airwallex/CoreTests/API/AWXPaymentIntentFunctionalTest.m
+++ Airwallex/CoreTests/API/AWXPaymentIntentFunctionalTest.m
@@ -14,6 +14,7 @@
 #import "AWXTestUtils.h"
 #import "XCTestCase+Utils.h"
 #import <XCTest/XCTest.h>
+#import <OCMock/OCMock.h>
 
 @interface AWXPaymentIntentFunctionalTest : XCTestCase
 
@@ -39,7 +40,16 @@
     request.intentId = self.paymentIntent.Id;
 
     AWXAPIClient *client = [[AWXAPIClient alloc] initWithConfiguration:[AWXAPIClientConfiguration sharedConfiguration]];
+    
+    id sessionMock = OCMClassMock([NSURLSession class]);
+    OCMStub([sessionMock sharedSession]).andReturn(sessionMock);
+    NSData *mockData = [@"MockData" dataUsingEncoding:NSUTF8StringEncoding];
+    id responseMock = OCMClassMock([NSHTTPURLResponse class]);
+    OCMStub([responseMock statusCode]).andReturn(200);
+    OCMStub([sessionMock dataTaskWithRequest:[OCMArg isKindOfClass:[NSMutableURLRequest class]] completionHandler:([OCMArg invokeBlockWithArgs:mockData, responseMock, [NSNull null], nil])]);
+    
     XCTestExpectation *expectation = [self expectationWithDescription:@"Retrieve payment intent"];
+
     [client send:request
          handler:^(AWXResponse *_Nullable response, NSError *_Nullable error) {
              XCTAssertNil(error);
