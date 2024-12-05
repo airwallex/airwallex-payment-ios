50a2fca68de52f52ebe3e1c62f4de91f94dbc3c8
weiping-awx <weiping.li@airwallex.com>
2024-12-05 10:08:49 +0800

    ci: APAM-97 add build phase to generate SPM headers (#109)
    * ci: add build phase to generate SPM public headers
    
    * ci: check  for build phase Run clang-format
    
    * ci: update Podfile.lock to use cocoapods 1.15.2 - same as CI virtual machine
    
    * ci: use CDN source for cocoapods to silence warning

---
+++ scripts/generate_spm_public_headers.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -e
+
+if [ -n "$CI" ]; then
+    echo "Skip - generate symbolic links for CI."
+    exit 0
+fi
+
+AIRWALLEX_CORE_PUBLIC_HEADER_PATH="${SRCROOT}/../Airwallex/Core/include"
+AIRWALLEX_CORE_SOURCE_RELATIVE_PATH="../Sources"
+
+rm -rf $AIRWALLEX_CORE_PUBLIC_HEADER_PATH/*.*
+
+echo "Generate symbolic links for AirwallexCore public headers under ${AIRWALLEX_CORE_PUBLIC_HEADER_PATH}"
+
+cd $AIRWALLEX_CORE_PUBLIC_HEADER_PATH
+public_headers_list=$(
+    find "$AIRWALLEX_CORE_SOURCE_RELATIVE_PATH" \
+        -type f -name "*.[h]" \
+        -not -path "$AIRWALLEX_CORE_SOURCE_RELATIVE_PATH/Internal/*" \
+        | sed "s| \([^/]\)|:\1|g"
+)
+
+for public_file in $public_headers_list; do
+    echo "symbolic link: $public_file"
+    file_to_link=$(echo $public_file | sed "s|:| |g")
+    ln -s $file_to_link
+done
+cd $SRCROOT
+
+echo "      Done"
