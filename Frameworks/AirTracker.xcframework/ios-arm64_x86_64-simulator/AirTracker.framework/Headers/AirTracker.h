623c027165496c6fca6838618d9baebea1b5d577
weiping.li <weiping.li@airwallex.com>
2024-12-17 14:25:25 +0800

    chore: add PrivacyInfo for AirTracker

--- Frameworks/AirTracker.xcframework/ios-arm64_x86_64-simulator/AirTracker.framework/Headers/AirTracker.h
+++ Frameworks/AirTracker.xcframework/ios-arm64_x86_64-simulator/AirTracker.framework/Headers/AirTracker.h
@@ -15,3 +15,5 @@ FOUNDATION_EXPORT double AirTrackerVersionNumber;
 FOUNDATION_EXPORT const unsigned char AirTrackerVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <AirTracker/PublicHeader.h>
+
+
