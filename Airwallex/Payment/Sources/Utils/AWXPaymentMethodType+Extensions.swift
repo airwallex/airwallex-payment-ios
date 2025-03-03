cd6806943c865bcb662680abc786ec8c423627dd
weiping-awx <weiping.li@airwallex.com>
2025-03-03 16:57:00 +0800

    chore: remove empty file

--- Airwallex/Payment/Sources/Utils/AWXPaymentMethodType+Extensions.swift
+++
@@ -1,9 +0,0 @@
-//
-//  AWXPaymentMethodType+Extensions.swift
-//  Airwallex
-//
-//  Created by Weiping Li on 2025/2/25.
-//  Copyright © 2025 Airwallex. All rights reserved.
-//
-
-import Foundation
