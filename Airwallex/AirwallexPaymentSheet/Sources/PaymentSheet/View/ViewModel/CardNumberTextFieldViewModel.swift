c594b5cad22b28b6c750c16144cc5cc6bf7df6a5
weiping-awx <weiping.li@airwallex.com>
2025-04-24 16:22:38 +0800

    chore: keep initial brand order

--- Airwallex/AirwallexPaymentSheet/Sources/PaymentSheet/View/ViewModel/CardNumberTextFieldViewModel.swift
+++ Airwallex/AirwallexPaymentSheet/Sources/PaymentSheet/View/ViewModel/CardNumberTextFieldViewModel.swift
@@ -21,11 +21,29 @@ class CardNumberTextFieldViewModel: InfoCollectorTextFieldViewModel, CardBrandVi
         }
     }
     
+    private lazy var brandSortingDict: [AWXBrandType: Int] = {
+        var dict = [AWXBrandType: Int]()
+        for (idx, brand) in supportedBrands.enumerated() {
+            guard dict[brand] == nil else { continue }
+            dict[brand] = idx
+        }
+        return dict
+    }()
+    
     var cardBrands: [AWXBrandType] {
         if (text ?? "").isEmpty {
             return supportedBrands
         }
-        return formatter.candidates.filter { supportedBrands.contains($0) }
+        let results = formatter.candidates
+            .filter { supportedBrands.contains($0) }
+            .sorted {
+                guard let idx_l = brandSortingDict[$0],
+                      let idx_r = brandSortingDict[$1] else {
+                    return true
+                }
+                return idx_l <= idx_r
+            }
+        return results
     }
     
     private let formatter: CardNumberFormatter
